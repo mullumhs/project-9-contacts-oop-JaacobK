355b508d63a22ec6e630de7b33b56d541cb41c4a
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-05-13 09:49:41 +1000

    lab 2, 1

--- lab1.py
+++ lab1.py
@@ -13,6 +13,7 @@ class Car:
         self.color = color
         self.brand = brand
         self.is_engine_on = False
+        self.odometer = 0
 
     def start_engine(self):
         self.is_engine_on = True
@@ -22,6 +23,9 @@ class Car:
         self.is_engine_on = False
         print(f"The engine of the {self.color} {self.brand} is now off.")
 
+    def Drive(self, distance):
+        self.odometer += self.distance
+
 
 # Add another property to the Car class called "odometer".
 # This property should be initialised to 0.
@@ -30,14 +34,17 @@ class Car:
 
 # Create two Car objects. One should be a red Toyota and the other a blue Ford.
 
-
+Car1 = Car("red", "Toyota")
+Car2 = Car("blue", "Ford")
 
 # Start the engine of the red Toyota.
 
-
+Car1.start_engine()
 
 # Create a method called "drive" that takes a distance as a parameter.
 # The car can only be driven if the engine is on.
+
+def Drive():
 
 
 
--- lab2.py
+++ lab2.py
@@ -11,13 +11,20 @@
 # This class should have an initialiser with attributes for name, phone_number, and email.
 # Add a class attribute to keep track of the total number of contacts.
 
+class Contact:
+    total_contacts = 0
 
+    def __init__(self, name, Pnumber, email):
+        self.name = name
+        self.Pnumber = Pnumber
+        self.email = email
+        Contact.total_contacts += 1
 
 # Create at least two instances of the Contact class with different data.
 
-
-
+Contact1 = Contact("joe", "0478230984", "joe@gmail.com")
+Contact2 = Contact("bob", "0482193746", "bob@gmail.com")
 # Write code that prints out the details of each contact you have created.
 
-
-
+print(Contact1.name, Contact1.Pnumber, Contact1.email)
+print(Contact2.name, Contact2.Pnumber, Contact2.email)
