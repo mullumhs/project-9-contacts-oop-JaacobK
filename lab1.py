57898858a7dac3e7dda2c06666ef85897024a442
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-05-14 14:56:32 +1000

    lab 1 and 3 done

--- lab1.py
+++ lab1.py
@@ -24,7 +24,9 @@ class Car:
         print(f"The engine of the {self.color} {self.brand} is now off.")
 
     def Drive(self, distance):
-        self.odometer += self.distance
+        if self.is_engine_on == True:
+            self.odometer += distance
+            print(self.odometer)
 
 
 # Add another property to the Car class called "odometer".
@@ -44,14 +46,14 @@ Car1.start_engine()
 # Create a method called "drive" that takes a distance as a parameter.
 # The car can only be driven if the engine is on.
 
-def Drive():
-
 
 
 # Attempt to drive both cars 100Km.
 
-
+Car1.Drive(100)
+Car2.Drive(100)
 
 # Print the brand, odometer and colour of both cars.
 
-
+print(Car1.color, Car1.brand, Car1.odometer)
+print(Car2.color, Car2.brand, Car2.odometer)
--- lab3.py
+++ lab3.py
@@ -13,3 +13,36 @@
 # 4. Reproduce the instances of the Contact class that you created in Lab 2
 # 5. Call your new instance method on one Contact and print the result
 # 6. Use the class method to print out the total number of contacts
+
+class Contact:
+    total_contacts = 0
+
+    def __init__(self, name, Pnumber, email):
+        self.name = name
+        self.Pnumber = Pnumber
+        self.email = email
+        Contact.total_contacts += 1
+
+    def check_email(self):
+        if "@" in self.email:
+            print(f"{self.email} is valid")
+            return True
+        else:
+            print(f"{self.email} is not valid")
+            return False
+
+    @classmethod
+    def get_contact_count(cls):
+        return Contact.total_contacts
+
+
+
+Contact1 = Contact("joe", "0478230984", "joe@gmail.com")
+Contact2 = Contact("bob", "0482193746", "bob@gmail.com")
+
+print(Contact1.name, Contact1.Pnumber, Contact1.email)
+print(Contact2.name, Contact2.Pnumber, Contact2.email)
+
+Contact1.check_email()
+
+print(Contact.get_contact_count())
